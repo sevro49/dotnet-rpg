acab7743894e099b44349505a0041ca38cfcb2f1
sevro49 <21120205303@ismu.edu.tr>
2023-10-17 09:55:06 +0300

    Delete a character

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -30,6 +30,28 @@ namespace dotnet_rpg.Services.CharacterService
             return serviceResponse;
         }
 
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+
+            try
+            {
+                var character = characters.First(c => c.Id == id);
+                if (character is null)
+                    throw new Exception($"Character with Id '{id}' not found");
+
+                characters.Remove(character);
+
+                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
+
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
@@ -52,7 +74,7 @@ namespace dotnet_rpg.Services.CharacterService
             try
             {
                 var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
-                if(character is null)
+                if (character is null)
                     throw new Exception($"Character with Id '{updatedCharacter.Id}' not found");
 
                 character.Name = updatedCharacter.Name;
